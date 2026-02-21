923497747d772d777c58724a11b27d6c128d6432
John Rovin Amido <johnrovin06@gmail.com>
2026-02-22 05:16:53 +0800

    Added functionality to manage grades for subjects
    Update the grades attribute using the Map, so we can utilize the HashMap and we can have multiple subjects for each student and we can edit each subject's grade.

--- src/com/johnrovin/studentenrollmentandmanagementsystem/model/Student.java
+++ src/com/johnrovin/studentenrollmentandmanagementsystem/model/Student.java
@@ -1,5 +1,9 @@
 package com.johnrovin.studentenrollmentandmanagementsystem.model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Student {
 
   private final String id;
@@ -8,14 +12,39 @@ public class Student {
   private final int age;
   private final Course course;
   private Double grade;
+  private Map<Subject, Double> grades;
 
-  public Student(String id, String lastName, String firstName, int age, Course course) {
+  ArrayList<Subject> subjects;
+
+  public Student(String id, String lastName, String firstName, int age, Course course){
     this.id = id;
     this.lastName = lastName;
     this.firstName = firstName;
     this.age = age;
     this.course = course;
     this.grade = null;
+    subjects = new ArrayList<>(course.getSubjects());
+    this.grades = new HashMap<>();
+    for (Subject subject : subjects) {
+      grades.put(subject, null);
+    }
+  }
+
+  public void assignGrade(Subject subject, double grade){
+    if(grades.containsKey(subject)){
+      grades.put(subject, grade);
+    }
+  }
+
+  public void showGrades(){
+    for(Subject subject : subjects){
+      Double grade = grades.get(subject);
+      System.out.println(subject + " : " + (grade == null ? "Not graded yet" : grade));
+    }
+  }
+
+  public ArrayList<Subject> getSubjects() {
+    return subjects;
   }
 
   // Getters
@@ -40,8 +69,7 @@ public class Student {
 
   @Override
   public String toString() {
-    String studentName = lastName + ", " + firstName;
     return """
-     ID: %s | Student: %s | Age: %d | Course: %s | Grade: %.2f""".formatted(id, studentName, age, course, grade);
+     ID: %s | Student: %s, %s | Age: %d | Course: %s""".formatted(id, lastName, firstName, age, course);
   }
 }
